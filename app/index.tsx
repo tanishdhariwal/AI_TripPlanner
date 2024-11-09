4537cdc9327e3a4965b6eb02ad6f1fdd47db98c8
Tanish Dhariwal <tanishdhariwal21@gmail.com>
2024-11-09 18:21:52 +0530

    testting

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View } from "react-native";
+import Login from '../components/Login'
+import {auth} from './../configs/Firebase_Config'
+import { Redirect } from "expo-router";
+export default function Index() {
+
+  const user = auth.currentUser;
+  return (
+    <View
+      style={{
+        flex: 1
+
+      }}
+    >
+      {user?
+      <Redirect href={'/myTrip'}/>:
+      <Login/>
+      }
+    </View>
+  );
+}
